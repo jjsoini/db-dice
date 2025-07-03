ba2905581cca14491a45fd6597e092d419098fd4
Jussi Soini <jussisoini@Mac.home>
2025-07-03 21:36:46 +0300

    removed yarn.lock

--- src/meshes/DiceMesh.tsx
+++ src/meshes/DiceMesh.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import { DiceType } from "../types/DiceType";
 import { RoundedDiceMesh } from "./rounded/RoundedDiceMesh";
 import { SharpDiceMesh } from "./sharp/SharpDiceMesh";
+import { Group } from "three";
 
 type DiceMeshProps = JSX.IntrinsicElements["group"] & {
   diceType: DiceType;
   sharp?: boolean;
 };
 
-export const DiceMesh = React.forwardRef<THREE.Group, DiceMeshProps>(
+export const DiceMesh = React.forwardRef<Group, DiceMeshProps>(
   ({ sharp, ...props }, ref) => {
     if (sharp) {
       return <SharpDiceMesh ref={ref} {...props} />;
